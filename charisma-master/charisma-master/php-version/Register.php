8e936dea81c77a762edae37620dfdb3282010c75
Mahehsh Udara <maheshudara244@gmail.com>
2015-07-01 18:51:50 +0530

    Register

---
+++ charisma-master/charisma-master/php-version/Register.php
@@ -0,0 +1,50 @@
+<?php include 'Header.php';?>
+<?php include 'NavBar.php';?>
+<form role="form" xmlns="http://www.w3.org/1999/html">
+    <div class="col-sm-3"
+    <div class="form-group">
+
+
+        <label class="control-label col-sm-4" for="email">Email:</label>
+
+    <input type="email" class="form-control" id="email" placeholder="Enter email">
+
+        </div>
+    </div>
+
+
+</form>
+
+<div class="form-group">
+    <div class="row featurette">
+        <div class="col-md-3">
+            <input class="form-control input-lg" type="text" id="inputLarge"
+                   value="" placeholder="Email Address">
+
+        </div>
+        <div class="col-md-3 ">
+            <input class="form-control input-lg" type="text" id="email"
+                   value="" placeholder="Repeat Email Address">
+
+        </div>
+
+        <div class="col-md-3 ">
+            <input class="form-control input-lg" type="password" id="lName"
+                   value="" placeholder="Password">
+        </div>
+
+        <div class="col-md-3 ">
+            <button type="button" class="btn btn-primary btn-lg">Register
+                Today</button>
+            <a href="#myModal" role="button" id="myButt"
+               class="btn btn-large btn-primary" data-toggle="modal">Launch
+                Demo Modal</a>
+
+        </div>
+
+
+    </div>
+
+
+
+</div>
